d79cc22a349e51055ee173c43e556837dd92161b
Edouard DUPIN <yui.heero@gmail.com>
2016-05-03 21:36:48 +0200

    [DEV] first integration only availlable on linux

---
+++ lutin_curl-bin.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+import lutin.module as module
+import lutin.tools as tools
+import os
+
+
+def get_type():
+	return "BINARY"
+
+def get_desc():
+	return "cURL binary"
+
+def get_licence():
+	return "MIT"
+
+def get_compagny_type():
+	return "org"
+
+def get_compagny_name():
+	return "haxx"
+
+def get_maintainer():
+	return ["Daniel Stenberg<daniel@haxx.se>"]
+
+def get_version():
+	return [7,48,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
+	my_module.add_src_file([
+	    'curl/src/slist_wc.c',
+	    'curl/src/tool_binmode.c',
+	    'curl/src/tool_bname.c',
+	    'curl/src/tool_cb_dbg.c',
+	    'curl/src/tool_cb_hdr.c',
+	    'curl/src/tool_cb_prg.c',
+	    'curl/src/tool_cb_rea.c',
+	    'curl/src/tool_cb_see.c',
+	    'curl/src/tool_cb_wrt.c',
+	    'curl/src/tool_cfgable.c',
+	    'curl/src/tool_convert.c',
+	    'curl/src/tool_dirhie.c',
+	    'curl/src/tool_doswin.c',
+	    'curl/src/tool_easysrc.c',
+	    'curl/src/tool_formparse.c',
+	    'curl/src/tool_getparam.c',
+	    'curl/src/tool_getpass.c',
+	    'curl/src/tool_help.c',
+	    'curl/src/tool_helpers.c',
+	    'curl/src/tool_homedir.c',
+	    #'curl/src/tool_hugehelp.c',
+	    'generate/tool_hugehelp.c',
+	    'curl/src/tool_libinfo.c',
+	    'curl/src/tool_main.c',
+	    'curl/src/tool_metalink.c',
+	    'curl/src/tool_mfiles.c',
+	    'curl/src/tool_msgs.c',
+	    'curl/src/tool_operate.c',
+	    'curl/src/tool_operhlp.c',
+	    'curl/src/tool_panykey.c',
+	    'curl/src/tool_paramhlp.c',
+	    'curl/src/tool_parsecfg.c',
+	    'curl/src/tool_strdup.c',
+	    'curl/src/tool_setopt.c',
+	    'curl/src/tool_sleep.c',
+	    'curl/src/tool_urlglob.c',
+	    'curl/src/tool_util.c',
+	    'curl/src/tool_vms.c',
+	    'curl/src/tool_writeenv.c',
+	    'curl/src/tool_writeout.c',
+	    'curl/src/tool_xattr.c',
+	    'curl/lib/strtoofft.c',
+	    'curl/lib/rawstr.c',
+	    'curl/lib/nonblock.c',
+	    'curl/lib/warnless.c',
+	    ])
+	my_module.compile_flags('c', [
+	    '-DHAVE_CONFIG_H',
+	    ])
+	my_module.compile_flags('link', [
+	    '-lidn',
+	    '-llber',
+	    '-lldap',
+	    '-ldl',
+	    '-lssl',
+	    '-lcrypto',
+	    '-lz',
+	    '-lssh2',
+	    ])
+	    
+	my_module.compile_version("c", 1989, gnu=True)
+	my_module.add_module_depend('curl')
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "curl", "src"))
+	# Bad lib implementation ...
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "curl", "lib"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "curl", "include"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "curl", "include", "curl"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "generate"))
+	# end bad
+	return my_module
+
+"""
+',/home/heero/dev/perso/framework/curl-lutin/curl/src',,
+-DHAVE_CONFIG_H
+-I/home/heero/dev/perso/framework/curl-lutin/curl/include/curl
+-I/home/heero/dev/perso/framework/curl-lutin/curl/include
+-I/home/heero/dev/perso/framework/curl-lutin/curl/lib
+-I/home/heero/dev/perso/framework/curl-lutin/curl/src
+
+"""
+
+
---
+++ lutin_curl.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+import lutin.module as module
+import lutin.tools as tools
+import os
+
+
+def get_type():
+	return "LIBRARY"
+
+def get_desc():
+	return "cURL library"
+
+def get_licence():
+	return "MIT"
+
+def get_compagny_type():
+	return "org"
+
+def get_compagny_name():
+	return "haxx"
+
+def get_maintainer():
+	return ["Daniel Stenberg<daniel@haxx.se>"]
+
+def get_version():
+	return [7,48,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
+	my_module.add_src_file([
+	    'curl/lib/file.c',
+	    'curl/lib/timeval.c',
+	    'curl/lib/base64.c',
+	    'curl/lib/hostip.c',
+	    'curl/lib/progress.c',
+	    'curl/lib/formdata.c',
+	    'curl/lib/cookie.c',
+	    'curl/lib/http.c',
+	    'curl/lib/sendf.c',
+	    'curl/lib/ftp.c',
+	    'curl/lib/url.c',
+	    'curl/lib/dict.c',
+	    'curl/lib/if2ip.c',
+	    'curl/lib/speedcheck.c',
+	    'curl/lib/ldap.c',
+	    'curl/lib/version.c',
+	    'curl/lib/getenv.c',
+	    'curl/lib/escape.c',
+	    'curl/lib/mprintf.c',
+	    'curl/lib/telnet.c',
+	    'curl/lib/netrc.c',
+	    'curl/lib/getinfo.c',
+	    'curl/lib/transfer.c',
+	    'curl/lib/strequal.c',
+	    'curl/lib/easy.c',
+	    'curl/lib/security.c',
+	    'curl/lib/curl_fnmatch.c',
+	    'curl/lib/fileinfo.c',
+	    'curl/lib/ftplistparser.c',
+	    'curl/lib/wildcard.c',
+	    'curl/lib/krb5.c',
+	    'curl/lib/memdebug.c',
+	    'curl/lib/http_chunks.c',
+	    'curl/lib/strtok.c',
+	    'curl/lib/connect.c',
+	    'curl/lib/llist.c',
+	    'curl/lib/hash.c',
+	    'curl/lib/multi.c',
+	    'curl/lib/content_encoding.c',
+	    'curl/lib/share.c',
+	    'curl/lib/http_digest.c',
+	    'curl/lib/md4.c',
+	    'curl/lib/md5.c',
+	    'curl/lib/http_negotiate.c',
+	    'curl/lib/inet_pton.c',
+	    'curl/lib/strtoofft.c',
+	    'curl/lib/strerror.c',
+	    'curl/lib/amigaos.c',
+	    'curl/lib/hostasyn.c',
+	    'curl/lib/hostip4.c',
+	    'curl/lib/hostip6.c',
+	    'curl/lib/hostsyn.c',
+	    'curl/lib/inet_ntop.c',
+	    'curl/lib/parsedate.c',
+	    'curl/lib/select.c',
+	    'curl/lib/tftp.c',
+	    'curl/lib/splay.c',
+	    'curl/lib/strdup.c',
+	    'curl/lib/socks.c',
+	    'curl/lib/ssh.c',
+	    'curl/lib/rawstr.c',
+	    'curl/lib/curl_addrinfo.c',
+	    'curl/lib/socks_gssapi.c',
+	    'curl/lib/socks_sspi.c',
+	    'curl/lib/curl_sspi.c',
+	    'curl/lib/slist.c',
+	    'curl/lib/nonblock.c',
+	    'curl/lib/curl_memrchr.c',
+	    'curl/lib/imap.c',
+	    'curl/lib/pop3.c',
+	    'curl/lib/smtp.c',
+	    'curl/lib/pingpong.c',
+	    'curl/lib/rtsp.c',
+	    'curl/lib/curl_threads.c',
+	    'curl/lib/warnless.c',
+	    'curl/lib/hmac.c',
+	    'curl/lib/curl_rtmp.c',
+	    'curl/lib/openldap.c',
+	    'curl/lib/curl_gethostname.c',
+	    'curl/lib/gopher.c',
+	    'curl/lib/idn_win32.c',
+	    'curl/lib/http_negotiate_sspi.c',
+	    'curl/lib/http_proxy.c',
+	    'curl/lib/non-ascii.c',
+	    'curl/lib/asyn-ares.c',
+	    'curl/lib/asyn-thread.c',
+	    'curl/lib/curl_gssapi.c',
+	    'curl/lib/curl_ntlm.c',
+	    'curl/lib/curl_ntlm_wb.c',
+	    'curl/lib/curl_ntlm_core.c',
+	    'curl/lib/curl_ntlm_msgs.c',
+	    'curl/lib/curl_sasl.c',
+	    'curl/lib/curl_multibyte.c',
+	    'curl/lib/hostcheck.c',
+	    'curl/lib/conncache.c',
+	    'curl/lib/pipeline.c',
+	    'curl/lib/dotdot.c',
+	    'curl/lib/x509asn1.c',
+	    'curl/lib/http2.c',
+	    'curl/lib/curl_sasl_sspi.c',
+	    'curl/lib/smb.c',
+	    'curl/lib/curl_sasl_gssapi.c',
+	    'curl/lib/curl_endian.c',
+	    'curl/lib/curl_des.c',
+	    'curl/lib/vtls/openssl.c',
+	    'curl/lib/vtls/gtls.c',
+	    'curl/lib/vtls/vtls.c',
+	    'curl/lib/vtls/nss.c',
+	    'curl/lib/vtls/polarssl.c',
+	    'curl/lib/vtls/polarssl_threadlock.c',
+	    'curl/lib/vtls/axtls.c',
+	    'curl/lib/vtls/cyassl.c',
+	    'curl/lib/vtls/schannel.c',
+	    'curl/lib/vtls/darwinssl.c',
+	    'curl/lib/vtls/gskit.c',
+	    'curl/lib/vtls/mbedtls.c',
+	    ])
+	my_module.compile_flags('c', [
+	    '-Dlibcurl_EXPORTS',
+	    '-DBUILDING_LIBCURL',
+	    '-DHAVE_CONFIG_H',
+	    ])
+	my_module.compile_version("c", 1989, gnu=True)
+	my_module.add_module_depend('z')
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "curl", "lib"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "curl", "include"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "curl", "include", "curl"))
+	my_module.add_path(os.path.join(tools.get_current_path(__file__), "generate"))
+	my_module.add_header_file([
+	    'curl/include/curl/mprintf.h',
+	    'curl/include/curl/curlver.h',
+	    'curl/include/curl/multi.h',
+	    'curl/include/curl/curl.h',
+	    'curl/include/curl/curlrules.h',
+	    'curl/include/curl/typecheck-gcc.h',
+	    'curl/include/curl/easy.h',
+	    'curl/lib/curl_setup.h',
+	    'generate/curlbuild.h',
+	    ],
+	    destination_path="curl")
+	return my_module
